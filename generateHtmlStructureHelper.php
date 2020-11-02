6434c5b95a36b9a6bb8e98b05fa0750379cac662
oscar <oscar@des13.com>
2020-11-02 11:26:11 +0800

    fix: 📄 update PHP Headers license information

--- generateHtmlStructureHelper.php
+++ generateHtmlStructureHelper.php
@@ -2,7 +2,7 @@
 
 /**
  * @title        Mijoshop Admin Menu
- * @copyright   	Copyright (C) 2020 Esangtek, All rights reserved.
+ * @copyright    Copyright (C) 2020 Esangtek, All rights reserved.
  * @license      GNU General Public License version 3 or later.
  * @author url   https://des13.com/
  * @developers   Oscar Lin
